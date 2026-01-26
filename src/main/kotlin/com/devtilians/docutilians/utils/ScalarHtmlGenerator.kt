13fa502c5c1e0fec7e42da62ec536ba29cae00c0
ghdcksgml1 <ghdcksgml2@naver.com>
2026-01-26 22:09:31 +0900

    feat: enhance HTML generation with improved doctype and meta tags

--- src/main/kotlin/com/devtilians/docutilians/utils/ScalarHtmlGenerator.kt
+++ src/main/kotlin/com/devtilians/docutilians/utils/ScalarHtmlGenerator.kt
@@ -4,11 +4,14 @@ object ScalarHtmlGenerator {
 
     fun generateHtmlContent(openApiYamlFilename: String): String {
         return """
-            <!DOCTYPE html>
-            <html>
+            <!doctype html>
+            <html lang="en">
             <head>
-                <title>API Docs</title>
-                <meta charset="utf-8"/>
+                <meta charset="UTF-8">
+                <meta name="viewport"
+                      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>Document</title>
             </head>
             <body>
             <script
